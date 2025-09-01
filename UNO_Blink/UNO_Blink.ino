63937db1903c2c0e93aa4c0f663e46789e7a0785
asrlab-code <amaritsarus@gmail.com>
2025-09-01 22:01:11 +0700

    Add files via upload
    Modified UNO_Blink.ino

--- UNO_Blink/UNO_Blink.ino
+++ UNO_Blink/UNO_Blink.ino
@@ -1,8 +1,15 @@
+#define DELAY_TIME 250
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-  delay(250);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(DELAY_TIME);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(DELAY_TIME);
+
+  //digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  //delay(DELAY_TIME);
 }
